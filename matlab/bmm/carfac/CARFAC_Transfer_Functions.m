410c6d72618511af9be8250a51017b041a9af373
dicklyon@google.com <dicklyon@google.com@47b1ee86-127b-13bb-b609-7b595106ed26>
2012-03-07 23:18:32 +0000

    added some missing headers
    git-svn-id: http://aimc.googlecode.com/svn/trunk@199 47b1ee86-127b-13bb-b609-7b595106ed26

--- matlab/bmm/carfac/CARFAC_Transfer_Functions.m
+++ matlab/bmm/carfac/CARFAC_Transfer_Functions.m
@@ -1,3 +1,22 @@
+% Copyright 2012, Google, Inc.
+% Author: Richard F. Lyon
+%
+% This Matlab file is part of an implementation of Lyon's cochlear model:
+% "Cascade of Asymmetric Resonators with Fast-Acting Compression"
+% to supplement Lyon's upcoming book "Human and Machine Hearing"
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%     http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
 function [complex_transfns_freqs, ...
   stage_numerators, stage_denominators] = ...
   CARFAC_Transfer_Functions(CF, freqs, to_channels, from_channels)
