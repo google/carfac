f2ec2b089b913251bf2b5ffeec09476c6b29e339
dicklyon@google.com <dicklyon@google.com@47b1ee86-127b-13bb-b609-7b595106ed26>
2013-05-09 04:00:25 +0000

    And here's the function that run ffmpeg to make the movie from PNGs.
    git-svn-id: http://aimc.googlecode.com/svn/trunk@265 47b1ee86-127b-13bb-b609-7b595106ed26

---
+++ matlab/bmm/carfac/MakeMovieFromPngsAndWav.m
@@ -0,0 +1,36 @@
+% Copyright 2013, Google, Inc.
+% Author: Richard F. Lyon
+%
+% This Matlab file is part of an implementation of Lyon's cochlear model:
+% "Cascade of Asymmetric Resonators with Fast-Acting Compression"
+% to supplement Lyon's upcoming book "Human and Machine Hearing"
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%     http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
+function MakeMovieFromPngsAndWav(frame_rate, png_name_pattern, ...
+  wav_filename, out_filename)
+
+system(['rm ', out_filename]);
+
+if ~exist(wav_filename, 'file')
+  error('wave file is missing', wav_filename)
+end
+
+ffmpeg_command = ['/opt/local/bin/ffmpeg' ...
+  ' -r ' num2str(frame_rate) ...
+  ' -i ' png_name_pattern ...
+  ' -i ' wav_filename ...
+  ' -b:v 1024k' ...
+  ' ' out_filename];
+
+system(ffmpeg_command);
