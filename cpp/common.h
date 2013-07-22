4876d8332dc644ac35657e7b43ef8449931e00d9
Ron Weiss <ronw@google.com>
2013-07-22 14:51:28 -0700

    Don't re-define DISALLOW_COPY_AND_ASSIGN.

--- cpp/common.h
+++ cpp/common.h
@@ -36,8 +36,10 @@ static const FPType kPi = 3.141592653589793238;
 
 // This macro disallows the copy constructor and operator= functions.
 // This should be used in the private: declarations for a class.
+#ifndef DISALLOW_COPY_AND_ASSIGN
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
   TypeName(const TypeName&);               \
   void operator=(const TypeName&)
+#endif
 
 #endif  // CARFAC_COMMON_H
