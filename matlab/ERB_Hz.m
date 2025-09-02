def05c11cdb665e5994ffe217fbd9811fa0bb146
Michael Cui <mlcui@google.com>
2025-09-01 21:13:28 -0700

    Remove tab character from ERB_Hz.m
    We should not be using tab characters in this codebase.
    
    PiperOrigin-RevId: 801974883

--- matlab/ERB_Hz.m
+++ matlab/ERB_Hz.m
@@ -20,7 +20,7 @@ function ERB = ERB_Hz(CF_Hz, ERB_break_freq, ERB_Q)
 % function ERB = ERB_Hz(CF_Hz, ERB_break_freq, ERB_Q)
 %
 % Auditory filter nominal Equivalent Rectangular Bandwidth
-%	Ref: Glasberg and Moore: Hearing Research, 47 (1990), 103-138
+% Ref: Glasberg and Moore: Hearing Research, 47 (1990), 103-138
 % ERB = 24.7 * (1 + 4.37 * CF_Hz / 1000);
 
 if nargin < 3
